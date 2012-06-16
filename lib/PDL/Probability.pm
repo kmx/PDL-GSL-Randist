0092a8a416e29dc9de669c48d7f9f1595ccc6c94
T. Nishimura <tnish@fastmail.jp>
2012-06-16 02:29:26 -0700

    main Makefile.PL, PDL/Probability.pm stub

---
+++ lib/PDL/Probability.pm
@@ -0,0 +1,16 @@
+package PDL::Probability;
+use version; our $VERSION = qv('0.0.1');
+use strict;
+use warnings;
+#use 5.010_000;
+#use Data::Dumper;
+#use Carp;
+#use autodie;
+
+require Exporter;
+our @ISA = qw(Exporter);
+our @EXPORT_OK = qw();
+our @EXPORT = qw();
+
+1;
+
